3f272e58e47e420320d91b13588a8df8684dc8c7
Luciel Santos <luciel.santos@ubistart.com>
2020-09-09 03:09:32 -0300

    add config database

--- database/database.js
+++ database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiapress', 'root', 'admin', {
-  host: 'localhost',
+const connection = new Sequelize('heroku_1238e44da734034', 'b8e6cf29b4f864', '10d42779', {
+  host: 'us-cdbr-east-02.cleardb.com',
   dialect: 'mysql',
   timezone: '-03:00'
 });
